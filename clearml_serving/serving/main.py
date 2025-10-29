d214bfdcaea79429ff1a67b6d683755d70ce2d0d
Daniel Kelly <danielkelly89@googlemail.com>
2025-10-29 13:25:48 +0100

    fix: f-string syntax for Python 3.10 compatibility

--- clearml_serving/serving/main.py
+++ clearml_serving/serving/main.py
@@ -333,7 +333,7 @@ async def metrics_endpoint():
     return JSONResponse(status_code=200, content=metrics)
 
 router = APIRouter(
-    prefix=f"/{os.environ.get("CLEARML_DEFAULT_SERVE_SUFFIX", "serve")}",
+    prefix=f"/{os.environ.get('CLEARML_DEFAULT_SERVE_SUFFIX', 'serve')}",
     tags=["models"],
     responses={404: {"description": "Model Serving Endpoint Not found"}},
     route_class=GzipRoute,  # mark-out to remove support for GZip content encoding
